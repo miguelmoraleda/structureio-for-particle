de7355b2ee07774e73f9ed58bea771fb529a2f92
Miguel Moraleda <rayaslokas@gmail.com>
2016-02-29 21:20:32 -0300

    Renaming stuff to pass particle validation

---
+++ firmware/Structureioforspark.cpp
@@ -0,0 +1,11 @@
+#include "Structureioforspark.h"
+
+static const uint16_t TIMEOUT = 5000; // Allow maximum 5s between data packets.
+
+/**
+* Constructor.
+*/
+Structureioforspark::Structureioforspark()
+{
+
+}
